dfffa4edd0f48a2ca0deb084d3b71ab5cdee93b3
Kirill <kirillbad2004@yandex.ru>
2025-12-29 05:43:05 +0300

    fit(keys): show possible solutions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import angr
+import re
 from angr import SimState
 import claripy
 
@@ -9,6 +10,16 @@ def is_successful(state: SimState) -> bool:
 
 def is_failed(state: SimState) -> bool:
     return b"wrong" in state.posix.dumps(1) or b"is not valid" in state.posix.dumps(1)
+
+
+def show_solutions(
+    possible_solution: bytes, raw_output: bool | None = False
+) -> bytes | list[bytes]:
+    if raw_output:
+        return possible_solution
+
+    key_pattern = re.compile(b"\x41[^\x0a]{2}\x2d[^\x0a]{3}\x2d[^\x0a]{3}")
+    return key_pattern.findall(possible_solution)
 
 
 def solve_blind() -> str | None:
@@ -24,6 +35,18 @@ def solve_blind() -> str | None:
     simgr.explore(find=is_successful, avoid=is_failed)
 
     if simgr.found:
+        print(
+            *[
+                "".join(chr(byte) if 32 <= byte <= 126 else "?" for byte in solution)
+                for solution in show_solutions(
+                    possible_solution=simgr.found[0].solver.eval(
+                        input_bvs, cast_to=bytes
+                    )
+                )
+            ],
+            sep="\n",
+        )
+
         return simgr.found[0].posix.dumps(1).decode()
 
 
